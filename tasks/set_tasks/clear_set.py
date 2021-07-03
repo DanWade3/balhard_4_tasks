d6023ad0f92d99f0307f1b063b25292fe0332a5d
Daniil Portnov <danila.portnov.wade@gmail.com>
2021-07-03 20:05:55 +0300

    clear set done

--- tasks/set_tasks/clear_set.py
+++ tasks/set_tasks/clear_set.py
@@ -7,8 +7,8 @@
 
 def clear_set(collection: set) -> set:
     # TODO вставить код сюда
+    collection.clear()
     return collection
-
 
 if __name__ == '__main__':
     assert clear_set({1, 2, 3}) == set()
